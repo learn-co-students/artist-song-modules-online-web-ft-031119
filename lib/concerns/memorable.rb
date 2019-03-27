d4f47137e194e3cfa1ea56d4c75a6e1f17376e29
Alberto Morales <cmtmorales24@gmail.com>
2019-03-27 21:07:12 +0000

    Done.

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,14 +1,19 @@
 module Memorable 
-module ClassMethods
-
-def reset_all
+  module ClassMethods
+  
+    def reset_all
     self.all.clear
-end
+    end
 
- def count
-   self.all.count
- end
+    def count
+    self.all.count
+    end
+  
+  end 
+  module InstanceMethods
+       def initialize
+       self.class.all << self
+       end
 
-
-
+  end 
 end 
